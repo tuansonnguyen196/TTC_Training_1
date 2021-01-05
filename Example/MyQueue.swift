fd126dfa089a774da3291892364a4a795b6ce153
Lê Công <conglv1@ttc-solutions.com>
2021-01-05 16:26:40 +0700

    fix xcode 11

--- Example/MyQueue.swift
+++ Example/MyQueue.swift
@@ -9,28 +9,61 @@ import Foundation
 
 class Queue<T> {
     private var elements: [T] = []
+    private let queue = DispatchQueue(label: "vn.conglv.queue", attributes: .concurrent)
+    private let semaphore = DispatchSemaphore(value: 1)
+    private let lock = NSLock()
     
     func pushDispatch(_ element: T) {
-        self.elements.append(element)
+        queue.async(flags: .barrier) {
+            self.elements.append(element)
+        }
+        
     }
     
     func popDispatch() -> T? {
-        return self.elements.removeFirst()
+        queue.sync(flags: .barrier) {
+            if self.elements.isEmpty {
+                return nil
+            }
+            
+            return self.elements.removeFirst()
+        }
     }
     
     func pushSemaphore(_ element: T) {
+        semaphore.wait()
+        defer {
+            semaphore.signal()
+        }
+        
         self.elements.append(element)
     }
     
     func popSemaphore() -> T? {
+        semaphore.wait()
+        defer {
+            semaphore.signal()
+        }
+        
+        if self.elements.isEmpty {
+            return nil
+        }
         return self.elements.removeFirst()
     }
     
     func pushLock(_ element: T) {
+        lock.lock()
+        defer {
+            lock.unlock()
+        }
         self.elements.append(element)
     }
     
     func popLock() -> T? {
+        lock.lock()
+        defer {
+            lock.unlock()
+        }
         return self.elements.removeFirst()
     }
 }
